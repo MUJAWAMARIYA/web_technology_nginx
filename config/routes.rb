39856891934fbe9e18c40aa51210d75bee09fe63
MUJAWAMARIYA <>
2019-11-07 19:44:56 +0000

    nginx

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :blogs
     root 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
